692ecb8ac3c4ab8c57f5930adaf90e0953198dec
epruitt <epruitt223@gmail.com>
2026-06-02 21:09:52 -0500

    updated remote state file

---
+++ terraform-manifest/c3-remote-state.tf
@@ -0,0 +1,24 @@
+# Datasource
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-dev-us-east-2-x6n4tn"
+    key    = "vpc/dev/terraform.tfstate"
+    region = var.aws_region
+  }
+  
+}
+
+#outputs
+output "vpc_id" {
+  value = data.terraform_remote_state.vpc.outputs.vpc_id
+}
+
+output "private_subnet_ids" {
+  value = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+}
+
+output "public_subnet_ids" {
+  value = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+}
