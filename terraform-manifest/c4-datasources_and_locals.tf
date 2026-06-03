fc25e0e00736d79bca31dabce08fa4e6e5a2f2f3
epruitt <epruitt223@gmail.com>
2026-06-02 21:31:33 -0500

    updated datasources and locals file

---
+++ terraform-manifest/c4-datasources_and_locals.tf
@@ -0,0 +1,16 @@
+
+locals{
+
+  #Business division or team name (from variable)
+  owners = var.business_division
+
+  # Environment name such as dev, staging, prod (from variable)
+  environment = var.environment_name
+
+  #standardized naming prefix: "<division>-<env>"
+  name ="${local.name}-${local.environment}"  #ex: retail-dev
+
+  #Full EKS cluster name used for resource naming and tagging
+  eks_cluster_name ="${local.name}-${var.cluster_name}" #ex: reail-dev-eksdemo
+
+  }
