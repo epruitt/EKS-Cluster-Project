f0bb3dbce47783f6bb5f0fde4c2a03db5442a5d5
E Pruitt <epruitt223@gmail.com>
2026-06-09 21:27:27 -0500

    fix misspelling in eks tags file

--- terraform-manifest/c5-eks_tags.tf
+++ terraform-manifest/c5-eks_tags.tf
@@ -2,14 +2,14 @@
 resource "aws_ec2_tag" "eks_subnet_tag_public_elb" {
   for_each = toset(data.terraform_remote_state.vpc.outputs.public_subnet_ids)
   resource_id = each.value
-  key = "kubetnetes.io/role/elb"
+  key = "kubertnetes.io/role/elb"
   value = "1"
 }
 
 resource "aws_ec2_tag" "eks_subnet_tag_public_cluster" {
   for_each = toset(data.terraform_remote_state.vpc.outputs.public_subnet_ids)
   resource_id = each.value
-  key = "kubetnetes.io/cluster/${local.eks_cluster_name}"
+  key = "kubertnetes.io/cluster/${local.eks_cluster_name}"
   value = "shared"
 }
 
@@ -17,13 +17,13 @@ resource "aws_ec2_tag" "eks_subnet_tag_public_cluster" {
 resource "aws_ec2_tag" "eks_subnet_tag_private_elb" {
   for_each = toset(data.terraform_remote_state.vpc.outputs.private_subnet_ids)
   resource_id = each.value
-  key = "kubetnetes.io/role/internal-elb"
+  key = "kubertnetes.io/role/internal-elb"
   value = "1"
 }
 
 resource "aws_ec2_tag" "eks_subnet_tag_private_cluster" {
   for_each = toset(data.terraform_remote_state.vpc.outputs.private_subnet_ids)
   resource_id = each.value
-  key = "kubetnetes.io/cluster/${local.eks_cluster_name}"
+  key = "kubertnetes.io/cluster/${local.eks_cluster_name}"
   value = "shared"
 }
